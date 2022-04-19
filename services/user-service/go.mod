5810646f663ded9c22cd8d7ab63a0abdcd38bd8b
arvryna <arv.pyrna@gmail.com>
2022-04-19 21:42:24 +0300

    Nats connected

--- services/user-service/go.mod
+++ services/user-service/go.mod
@@ -2,9 +2,13 @@ module github.com/arvryna/betnomi/user-service
 
 go 1.17
 
+require github.com/nats-io/nats.go v1.14.0
+
 require (
-	github.com/nats-io/nats.go v1.14.0 // indirect
+	github.com/golang/protobuf v1.5.2 // indirect
+	github.com/nats-io/nats-server/v2 v2.8.0 // indirect
 	github.com/nats-io/nkeys v0.3.0 // indirect
 	github.com/nats-io/nuid v1.0.1 // indirect
-	golang.org/x/crypto v0.0.0-20210314154223-e6e6c4f2bb5b // indirect
+	golang.org/x/crypto v0.0.0-20220112180741-5e0467b6c7ce // indirect
+	google.golang.org/protobuf v1.28.0 // indirect
 )
--- services/user-service/main.go
+++ services/user-service/main.go
@@ -1,7 +1,42 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"time"
+
+	"github.com/nats-io/nats.go"
+)
+
+func connectNats() *nats.Conn {
+	nc, err := nats.Connect("nats://nats-server:4222")
+	if err != nil {
+		log.Fatal("Could not connect to nats")
+		panic(err)
+	} else {
+		log.Println("Nats connected...")
+	}
+	return nc
+}
+
+/*
+User service doesn't need to ask from transaction to solve some problems
+*/
 
 func main() {
-	fmt.Println("user service")
+	nc := connectNats()
+
+	// how will you retry if there is error ?
+	// how to handle NATS errors ?
+	msg, err := nc.Request("Ping.TransactionService", nil, 10*time.Millisecond)
+
+	if err != nil {
+		log.Fatal("Transervice Ping failed.. ", err)
+	} else {
+		log.Println("Ping reponse", string(msg.Data))
+	}
+
+	fmt.Println("user service starting..")
+
+	// add code to graceful shutdown
 }
