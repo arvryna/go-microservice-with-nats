08e8aa97c5fb18a935d0f4876716042e7f505a03
arvryna <arv.pyrna@gmail.com>
2022-04-19 22:12:35 +0300

    Added health check

--- services/transaction-service/main.go
+++ services/transaction-service/main.go
@@ -1,7 +1,34 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"time"
+
+	"github.com/nats-io/nats.go"
+)
+
+func connectNats() *nats.Conn {
+	nc, err := nats.Connect("nats://nats-server:4222")
+	if err != nil {
+		//TODO: add proper logging to identify service names
+		log.Fatal("Could not connect to nats")
+	} else {
+		log.Println("Nats connected...")
+	}
+	return nc
+}
 
 func main() {
-	fmt.Println("transaction service")
+	nc := connectNats()
+	nc.Subscribe("Ping.TransactionService", func(m *nats.Msg) {
+		if string(m.Data) == "Ping" {
+			m.Respond([]byte("Pong"))
+		}
+	})
+	fmt.Println("transaction service started...")
+
+	time.Sleep(1000 * time.Second)
+
+	//handling graceful shutdown
 }
--- services/user-service/main.go
+++ services/user-service/main.go
@@ -9,10 +9,10 @@ import (
 )
 
 func connectNats() *nats.Conn {
+	// User viper to import conf
 	nc, err := nats.Connect("nats://nats-server:4222")
 	if err != nil {
-		log.Fatal("Could not connect to nats")
-		panic(err)
+		log.Fatal("Could not connect to nats", err)
 	} else {
 		log.Println("Nats connected...")
 	}
@@ -23,20 +23,27 @@ func connectNats() *nats.Conn {
 User service doesn't need to ask from transaction to solve some problems
 */
 
+func performHealthCheck(nc *nats.Conn) {
+	for {
+		time.Sleep(2 * time.Second)
+		msg, err := nc.Request("Ping.TransactionService", []byte("Ping"), 1000*time.Millisecond)
+		if err != nil {
+			log.Println("heathcheck failed: ", err)
+		} else {
+			log.Print("healthcheck passed: ", string(msg.Data))
+		}
+	}
+}
+
 func main() {
 	nc := connectNats()
 
+	go performHealthCheck(nc)
+
 	// how will you retry if there is error ?
 	// how to handle NATS errors ?
-	msg, err := nc.Request("Ping.TransactionService", nil, 10*time.Millisecond)
-
-	if err != nil {
-		log.Fatal("Transervice Ping failed.. ", err)
-	} else {
-		log.Println("Ping reponse", string(msg.Data))
-	}
-
 	fmt.Println("user service starting..")
 
 	// add code to graceful shutdown
+	time.Sleep(10000 * time.Second)
 }
