64dc51beb1ab120945b3f64ab71e72576728aa61
arvryna <arv.pyrna@gmail.com>
2022-04-21 00:12:04 +0300

    Changes made on: Чт 21 апр 2022 00:12:04 MSK

--- services/user-service/go.mod
+++ services/user-service/go.mod
@@ -2,7 +2,12 @@ module github.com/arvryna/betnomi/user-service
 
 go 1.17
 
-require github.com/nats-io/nats.go v1.14.0
+require (
+	github.com/google/uuid v1.3.0
+	github.com/nats-io/nats.go v1.14.0
+	google.golang.org/grpc v1.45.0
+	google.golang.org/protobuf v1.28.0
+)
 
 require (
 	github.com/golang/protobuf v1.5.2 // indirect
@@ -10,5 +15,8 @@ require (
 	github.com/nats-io/nkeys v0.3.0 // indirect
 	github.com/nats-io/nuid v1.0.1 // indirect
 	golang.org/x/crypto v0.0.0-20220112180741-5e0467b6c7ce // indirect
-	google.golang.org/protobuf v1.28.0 // indirect
+	golang.org/x/net v0.0.0-20211112202133-69e39bad7dc2 // indirect
+	golang.org/x/sys v0.0.0-20220111092808-5a964db01320 // indirect
+	golang.org/x/text v0.3.6 // indirect
+	google.golang.org/genproto v0.0.0-20200526211855-cb27e3aa2013 // indirect
 )
--- services/user-service/main.go
+++ services/user-service/main.go
@@ -1,11 +1,16 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"log"
+	"net"
 	"time"
 
+	"github.com/arvryna/betnomi/user-service/usermanager"
+	"github.com/google/uuid"
 	"github.com/nats-io/nats.go"
+	"google.golang.org/grpc"
 )
 
 func connectNats() *nats.Conn {
@@ -22,7 +27,6 @@ func connectNats() *nats.Conn {
 /*
 User service doesn't need to ask from transaction to solve some problems
 */
-
 func performHealthCheck(nc *nats.Conn) {
 	for {
 		time.Sleep(2 * time.Second)
@@ -35,10 +39,37 @@ func performHealthCheck(nc *nats.Conn) {
 	}
 }
 
+// Grpc Stuffs
+type UserManagerServer struct {
+	usermanager.UnimplementedUserManagerServer
+}
+
+func (u *UserManagerServer) CreateUser(ctx context.Context, in *usermanager.NewUser) (*usermanager.User, error) {
+	log.Println("CreateUser gRPC request")
+	return &usermanager.User{Name: in.Name, Email: in.Email, Token: uuid.New().String()}, nil
+}
+
+func setupGRPCServer() {
+	listener, err := net.Listen("tcp", ":9101")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	s := grpc.NewServer()
+	usermanager.RegisterUserManagerServer(s, &UserManagerServer{})
+
+	err = s.Serve(listener)
+	if err != nil {
+		log.Fatal("GPRC server failed to start", err)
+	}
+}
+
 func main() {
 	nc := connectNats()
 
 	go performHealthCheck(nc)
+
+	go setupGRPCServer()
 
 	// how will you retry if there is error ?
 	// how to handle NATS errors ?
